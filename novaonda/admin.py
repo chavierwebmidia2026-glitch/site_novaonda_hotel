ebf78383909794fcbf33729f1205c82e886993b9
chavierwebmidia2026-glitch <chavierwebmidia2026@gmail.com>
2026-09-07 17:16:47 -0300

    Configure hotel menu admin
    - Registro do model MenuHotel no Django Admin
    - Configuração da listagem dos itens cadastrados
    - Adição de filtro para itens ativos
    - Configuração da ordenação dos itens pelo campo ordem
    - Criação do usuário administrador para acesso ao painel
    - Teste do cadastro dos itens do menu pelo Django Admin

--- novaonda/admin.py
+++ novaonda/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import MenuHotel
 
-# Register your models here.
+@admin.register(MenuHotel)
+class MenuHotelAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'ordem',
+        'icone',
+        'titulo',
+        'ativo',
+    )
+
+    list_filter = (
+        'ativo',
+    )
+
+    ordering = (
+        'ordem',
+    )
