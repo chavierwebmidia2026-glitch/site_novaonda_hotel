d7b6ee0092d480933cbf54241e0632dc65e93122
chavierwebmidia2026-glitch <chavierwebmidia2026@gmail.com>
2026-09-07 17:04:19 -0300

    Create hotel menu model
    - Criação do model MenuHotel
    - Adição dos campos título, ícone, URL, status e ordem
    - Criação da migration do novo model
    - Aplicação da migration no banco de dados
    - Teste da migration sem erros

--- novaonda/models.py
+++ novaonda/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class MenuHotel(models.Model):
+    titulo = models.CharField(max_length=100)
+
+    icone = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    url = models.CharField(
+        max_length=200,
+        blank=True
+    )
+
+    ativo = models.BooleanField(
+        default=True
+    )
+
+    ordem = models.PositiveIntegerField(
+        default=0
+    )
+
+    def __str__(self):
+        return self.titulo
